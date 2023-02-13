6f33b648555c58347d6fa5e71c08d7d5bea8134b
Jonathan Lyon <dallasstars2021@yahoo.com>
2023-02-13 17:43:25 -0500

    API blockchain & transaction endpoints

--- dev/api.js
+++ dev/api.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain(); //instance of our Blockchain constructor function
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/blockchain', function (req, res) {
-
+    res.send(bitcoin)
 })
 
 app.post('/transaction', function (req, res) {
-    console.log(req.body);
-    res.send(`Amount of the txn is ${req.body.amount} bitcoin`);
+   const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({note: `Transaction will be added in block ${blockIndex}.`});
 })
 
 app.get('/mine', function (req, res) {
